cb96f27d7c6835d7e779e1c3ebd383af001180c3
jnieves <jnieves@rsm.pr>
2021-07-01 17:51:39 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WShop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
